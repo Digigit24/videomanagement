d70775c2dd6fd316d179539e29bce23990d0bd69
Pratik <pratiknpawar4912@gmail.com>
2026-02-18 10:41:42 +0530

    Add localhost:3001 and localhost:5174 to CORS origins to fix dev environment issues

--- server/app.js
+++ server/app.js
@@ -13,6 +13,8 @@ app.use(
       "http://localhost:3000",
       "http://localhost:5173",
       "http://localhost:5000",
+      "http://localhost:3001",
+      "http://localhost:5174",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
